80b4196f5fb45ee7f647a8b77e33711073ca7648
Royer Adames <royeraadames@gmail.com>
2023-04-21 11:12:17 -0400

    add result summary component and its html

---
+++ src/app/result-summary/result-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result-summary',
+  templateUrl: './result-summary.component.html',
+  styleUrls: ['./result-summary.component.css']
+})
+export class ResultSummaryComponent {
+
+}
