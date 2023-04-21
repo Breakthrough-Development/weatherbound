f92302ddf1db124572fe9f582e1b300ec781de43
Royer Adames <royeraadames@gmail.com>
2023-04-21 15:13:32 -0400

    add alt text for images and

--- src/app/result-summary/data.ts
+++ src/app/result-summary/data.ts
@@ -4,7 +4,10 @@ export const Data: DataInterface[] = [
   {
     category: { text: 'Reaction', styles: ['text-red-500'] },
     score: 80,
-    icon: './assets/images/icon-reaction.svg',
+    icon: {
+      src: './assets/images/icon-reaction.svg',
+      alt: 'Thunder ray icon: a stylized image of a lightning bolt with a curved tail, resembling a ray fish.',
+    },
     maxScore: 100,
     styles: ['bg-pink-100'],
   },
@@ -12,21 +15,30 @@ export const Data: DataInterface[] = [
     category: { text: 'Memory', styles: ['text-yellow-400'] },
     score: 92,
     maxScore: 100,
-    icon: './assets/images/icon-memory.svg',
+    icon: {
+      src: './assets/images/icon-memory.svg',
+      alt: 'Human brain icon: a simplified, stylized image of a human brain, with a wavy outline and various lobes and folds depicted.',
+    },
     styles: ['bg-yellow-100'],
   },
   {
     category: { text: 'Verbal', styles: ['text-teal-500'] },
     score: 61,
     maxScore: 100,
-    icon: './assets/images/icon-verbal.svg',
+    icon: {
+      src: './assets/images/icon-verbal.svg',
+      alt: 'Message icon: a white speech bubble outlined in green, resembling the messaging icon used in the WhatsApp app.',
+    },
     styles: ['bg-green-100'],
   },
   {
     category: { text: 'Visual', styles: ['text-blue-900'] },
     maxScore: 100,
     score: 72,
-    icon: './assets/images/icon-visual.svg',
+    icon: {
+      src: './assets/images/icon-visual.svg',
+      alt: 'Human eye icon: a simplified, stylized image of a human eye, with a circular pupil, an iris with radial lines, and upper and lower eyelids depicted."',
+    },
     styles: ['bg-blue-50'],
   },
 ];
--- src/app/result-summary/models/DataInterface.ts
+++ src/app/result-summary/models/DataInterface.ts
@@ -1,7 +1,7 @@
 export interface DataInterface {
   category: { text: string; styles: StylesType };
   score: number;
-  icon: string;
+  icon: { src: string; alt: string };
   maxScore: number;
   styles: StylesType;
 }
