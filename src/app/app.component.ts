90f97b51280a11ff487643e961e9d87b37867ad0
Royer Adames <royeraadames@gmail.com>
2023-04-23 09:49:01 -0400

    toggle login and logout text; Add text logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'results-summary-component';
+  isLogin = false;
+
+  handleLogin(login: 'logout' | 'login') {
+    this.isLogin = !this.isLogin;
+    console.log(login);
+  }
 }
