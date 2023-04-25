11aef3eaaa3104a50beac83b1f03467061394a06
Royer Adames <royeraadames@gmail.com>
2023-04-25 19:00:56 -0400

    remove cookie service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResultSummaryComponent } from './result-summary/result-summary.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ResultSummaryComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ResultSummaryComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
