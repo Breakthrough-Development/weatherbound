a9dc8f985f925ad01e468b29d73e8a52408c3dcd
Royer Adames <royeraadames@gmail.com>
2023-04-25 19:14:18 -0400

    user is login when is verify and we update the user data base on the user subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthService } from './services/auth/auth.service';
 import { UserService } from './services/user/user.service';
+import { UserInterface } from './services/user/user.model';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { UserService } from './services/user/user.service';
 export class AppComponent implements OnInit {
   title = 'results-summary-component';
   isLogin = false;
+  user: UserInterface | null = null;
 
   constructor(
     private readonly cookieService: CookieService,
@@ -24,9 +26,15 @@ export class AppComponent implements OnInit {
       next: (response) => {
         console.log('User:', response);
         this.userService.user.next(response.data);
+        this.isLogin = true;
       },
       error: (error) => {
         console.error('Error during token verification:', error);
+      },
+    });
+    this.userService.user.subscribe({
+      next: (value): void => {
+        this.user = value;
       },
     });
   }
