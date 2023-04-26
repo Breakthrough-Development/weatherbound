4b7ac38d8bebc43d047959bb56abd2d2858ff502
Royer Adames <royeraadames@gmail.com>
2023-04-26 19:37:04 -0400

    add current endpoint data interface

---
+++ src/app/services/weather/models/current-data.interface.ts
@@ -0,0 +1,43 @@
+export interface CurrentDataInterface {
+  current: CurrentInterface;
+  location: LocationInterface;
+  request: RequestInterface;
+}
+
+interface CurrentInterface {
+  observation_time: string;
+  temperature: number;
+  weather_code: number;
+  weather_icons: string[];
+  weather_descriptions: string[];
+  wind_speed: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure: number;
+  precip: number;
+  humidity: number;
+  cloudcover: number;
+  feelslike: number;
+  uv_index: number;
+  visibility: number;
+  is_day: string;
+}
+
+interface LocationInterface {
+  name: string;
+  country: string;
+  region: string;
+  lat: string;
+  lon: string;
+  timezone_id: string;
+  localtime: string;
+  localtime_epoch: number;
+  utc_offset: string;
+}
+
+interface RequestInterface {
+  type: string;
+  query: string;
+  language: string;
+  unit: string;
+}
