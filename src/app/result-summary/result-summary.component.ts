88901a8ba3aa4f351f4505bdd8084ebf500fc003
Royer Adames <royeraadames@gmail.com>
2023-04-27 10:28:33 -0400

    get current weather data and display it

--- src/app/result-summary/result-summary.component.ts
+++ src/app/result-summary/result-summary.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Data } from './data';
 import { DataInterface } from './models/DataInterface';
+import { WeatherService } from '../services/weather/weather.service';
 
 @Component({
   selector: 'app-result-summary',
   templateUrl: './result-summary.component.html',
   styleUrls: ['./result-summary.component.css'],
 })
-export class ResultSummaryComponent {
+export class ResultSummaryComponent implements OnInit {
+  currentWeather = this.weatherService.currentData.value;
   data: DataInterface[] = Data;
+
+  constructor(private readonly weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.weatherService.currentData.subscribe((value) => {
+      this.currentWeather = value;
+    });
+  }
+  // location.name
+  // current.temperature
+  // current.weather_descriptions[0]
+  // current.weather_icon[0]
 }
