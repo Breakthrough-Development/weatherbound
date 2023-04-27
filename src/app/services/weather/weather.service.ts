694e9d9976a4c3242341b3df4df9553a5375ee13
Royer Adames <royeraadames@gmail.com>
2023-04-27 10:29:04 -0400

    store currentData in service

--- src/app/services/weather/weather.service.ts
+++ src/app/services/weather/weather.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs';
+import { BehaviorSubject, from, Observable } from 'rxjs';
 import axios, { AxiosResponse } from 'axios';
 import { environment } from '../../../environments/environment';
 import { WEATHER_ENDPOINTS } from './weather-endpoints';
+import { CurrentDataInterface } from './models/current-data.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
   endpoints = WEATHER_ENDPOINTS;
-
+  currentData: BehaviorSubject<CurrentDataInterface | null> =
+    new BehaviorSubject<CurrentDataInterface | null>(null);
   constructor() {}
 
   forecast(query: string): Observable<AxiosResponse<any>> {
