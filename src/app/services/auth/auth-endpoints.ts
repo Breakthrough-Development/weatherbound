2d337b056cd3214c47aa7be85cb4f5a44ae7abd4
Royer Adames <royeraadames@gmail.com>
2023-05-02 00:54:50 -0400

    google auth is fixed

--- src/app/services/auth/auth-endpoints.ts
+++ src/app/services/auth/auth-endpoints.ts
@@ -1,6 +1,5 @@
-//todo: make desktop/web dynamically load base on how the app starts
 export const AUTH_ENDPOINTS = {
-  googleAuth: 'auth/desktop/google',
+  googleAuth: 'auth/google',
   verify: 'auth/verify',
   callback: 'auth/callback',
   logout: 'auth/logout',
